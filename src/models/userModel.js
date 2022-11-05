4a42fb5a7baad0632f435ff21584f9885f9a43d1
jivan9822 <j.toshniwal@yahoo.co.in>
2022-11-05 18:19:58 +0800

    auth-2 assingment done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,21 +1,72 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      lowercase: true,
+      required: [true, "Please enter name of user"],
     },
-    emailId: String,
-    password: String,
+    address: {
+      street: String,
+      suite: String,
+      city: String,
+      zipcode: String,
+    },
+    phone: {
+      type: String,
+      required: [true, "Please enter your mobile number"],
+      unique: [true, "Number is already exists"],
+    },
+    website: String,
+    company: {
+      name: String,
+      catchPhrase: String,
+      bs: String,
+    },
+    email: {
+      type: String,
+      unique: [true, "Email id already exists"],
+      required: [true, "Please enter your mail id!"],
+    },
+    password: {
+      type: String,
+      required: [true, "Enter a password!"],
+      minlength: 10,
+      maxlength: 25,
+      select: false,
+    },
     gender: {
-        type: String,
-        enum: ["male", "female", "other"]
+      type: String,
+      enum: ["male", "female", "others"],
+    },
+    isDeleated: {
+      type: Boolean,
+      default: false,
     },
     age: Number,
-    posts: {type: [], deafult: []}
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+
+//         required: true
+//     },
+//     emailId: String,
+//     password: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "other"]
+//     },
+//     age: Number,
+//     posts: {type: [], deafult: []}
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController= require("../controllers/userController")
+
+const contClass = require("../controllers/classControler");
+const {
+  authorizectionCheck,
+  authCheck,
+} = require("../middleware/authNauthorization");
+const cont = new contClass();
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
-router.post("/users", userController.createUser)
+router.route("/createUser").post(cont.createUser);
 
-router.post("/login", userController.loginUser)
+router.route("/login").post(authCheck, cont.loginUser);
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router
+  .route("/user/:id")
+  .get(authorizectionCheck, cont.getUserDetails)
+    .patch(authorizectionCheck, cont.updateUserDetails)
+  .delete(authorizectionCheck, cont.deleteUser)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+// router.post("/users", userController.createUser)
+
+// router.post("/login", userController.loginUser)
+
+// //The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+// router.post("/users/:userId/posts", userController.postMessage)
+
+// router.put("/users/:userId", userController.updateUser)
+// router.delete('/users/:userId', userController.deleteUser)
 
 module.exports = router;
